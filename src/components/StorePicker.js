e3775ceed71137b254a56d72722d3c9c4d2046e6
fancyaction <labcoatdisaster@gmail.com>
2018-05-03 11:35:49 -0500

    Add lesson 5

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,13 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
 class StorePicker extends React.Component {
     render () {
-        return <p>I am the store picker!</p>
+        return (
+            <form className="store-selector">
+                <h2>Please enter a store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store</button>
+            </form>
+        )
     }
 }
 
