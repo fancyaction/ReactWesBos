0ff92fe2092c44ff9b3a6340a851e12222a7578f
fancyaction <labcoatdisaster@gmail.com>
2018-05-04 11:42:29 -0500

    Add lesson 14

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
-
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 class App extends React.Component {
-    state = {
-        fishes: {},
-        order: {}
-    };
-    addFish = fish => {
-        // 1. Make copy of existing state
-        const fishes = { ...this.state.fishes };
-        // 2. Add new fish to fishes variable
-        fishes[`fish${Date.now()}`] = fish;
-        // 3. Set new fishes object to state
-        this.setState({ fishes });
-    };
-
-    render() {
-      return (
-        <div className='catch-of-the-day'>
-            <div className="menu">
-                <Header tagline='Fresh seafood market'/>
-            </div>
-            <Order />
-            <Inventory addFish={this.addFish}/>
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // 1. Make copy of existing state
+    const fishes = { ...this.state.fishes };
+    // 2. Add new fish to fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set new fishes object to state
+    this.setState({ fishes });
+  };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh seafood market" />
         </div>
-      )
-    };
+        <Order />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
